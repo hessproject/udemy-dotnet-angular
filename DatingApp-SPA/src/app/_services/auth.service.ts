89e2aa6faea3b51fc467f39048c7d1d4dc004569
hessproject <hessproject@gmail.com>
2018-12-14 16:58:46 -0500

    Add membercard component, retrieve api data on client side

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  baseUrl = 'http://localhost:5000/api/auth/';
+  apiUrl = environment.apiUrl + environment.authEndpoint;
   loginEndpoint = 'login';
   registerEndpoint = 'register';
 
@@ -18,7 +19,7 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(model: any) {
-    return this.http.post(this.baseUrl + this.loginEndpoint, model)
+    return this.http.post(this.apiUrl + this.loginEndpoint, model)
       .pipe(
         map((response: any) => {
           const user = response;
@@ -36,7 +37,7 @@ export class AuthService {
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + this.registerEndpoint, model);
+    return this.http.post(this.apiUrl + this.registerEndpoint, model);
   }
 
 }
---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = environment.apiUrl + environment.usersEndpoint;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl, httpOptions);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(this.apiUrl + id, httpOptions);
+  }
+
+}
